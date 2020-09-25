64cbcab02044d2d2f92e23a89f9cbc4f31219c1c
cb-rohit.r <rohit.r@chargebee.com>
2020-09-26 00:58:11 +0530

    Setup Auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.min.css'
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+Amplify.configure(aws_exports);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
